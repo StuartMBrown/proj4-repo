83d2c4941609ab9fd9cb20feb34aa7bc4770c479
stephmcarlson <stephanie.carlson75@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email stephanie.carlson75@gmail.comgit config --global user.emailgit config --global user.email stephanie.carlson75@gmail.comgit config --global user.email stephanie.carlson75@gmail.com>
2024-02-22 21:24:23 -0600

    javascript starter file

---
+++ static/js/dashboard.js
@@ -0,0 +1,19 @@
+ 
+//  CSV Visualizations
+
+// Word Cloud for tweets
+    // https://www.jumpingrivers.com/blog/r-clickable-wordcloud-javascript-shiny/
+    // https://d3-graph-gallery.com/graph/wordcloud_basic
+
+// Bar graph of # of tweets by year
+    // https://www.jqueryscript.net/chart-graph/animated-column-bar-chart.html
+
+// Box with number of users
+    // https://docs.splunk.com/Documentation/Splunk/9.2.0/DashStudio/chartsSV
+
+// % of tweets that are positive
+    // https://stackoverflow.com/questions/48748638/how-to-create-single-value-doughnut-or-pie-chart-using-chart-js
+
+// User Input Field
+
+// Sentiment Score/Label Output
